3fc9383878316959e6d76f7285122876a05e5a17
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-05 00:01:12 -0400

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Mr. Chocolate',
+     36,
+     'M',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Rowdy',
+     36,
+     'F',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Tabitha',
+     36,
+     'F',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Sergeant Brown',
+     36,
+     'M',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Melissa',
+     36,
+     'F',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Grinch',
+     36,
+     'M',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     'Wendy',
+     36,
+     'F',
+     'blue',
+     'a',
+     TRUE
+);
+
+INSERT INTO bears ( name, age, sex, color, temperament, alive ) VALUES(
+     null,
+     36,
+     'M',
+     'blue',
+     'a',
+     TRUE
+);
